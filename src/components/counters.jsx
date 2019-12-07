56ab5eddb5b7a5d70d5a8c5b1908bb6485f28adf
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2019-12-07 22:32:55 +0530

    onclick for reset button

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -17,10 +17,23 @@ class Counters extends Component {
     this.setState({ counters: counters });
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
-        <button className="btn btn-primary btn-sm m-2">Reset</button>
+        <button
+          className="btn btn-primary btn-sm m-2"
+          onClick={this.handleReset}
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
